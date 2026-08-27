fe0b5167020ddad87722a5123a68aad117f7cdf2
douglasthepuglas <douglascannizzaro@pm.me>
2026-08-27 04:07:53 +0000

    Fixed lab01 project01

--- lab01/project01.c
+++ lab01/project01.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
+
 int main(void){
-    printf("       *\n      * \n     *  \n*   *   \n * *    \n  *     \n");
+    printf("       *\n      *\n     *  \n*   *\n * *\n  *\n");
     return (0);
 }
