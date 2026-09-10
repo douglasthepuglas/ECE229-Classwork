4af76e44e9b91ccd7dee4354fd007046a2c62345
douglasthepuglas <douglascannizzaro@pm.me>
2026-09-10 02:18:05 +0000

    Lab 5 done

--- lab05/lab05_project05.c
+++ lab05/lab05_project05.c
@@ -13,14 +13,12 @@
 char *letters[] = {"AEILNORSTU", "DG", "BCMP", "FHVWY", "K", "JX", "QZ"};
 int values[]     = {     1,        2,     3,      4,     5,    8,   10 };
 
-int value_of_word = 5;
+int value_of_word = 0;
 
 int letter_found_flag = 0;
 
 
 int main(void) {
-    printf("poo");
-    value_of_word = 0;
     char word[15];
     printf("Enter scrabble word: ");
     scanf("%s", word);
@@ -29,11 +27,11 @@ int main(void) {
         word[i] = toupper(word[i]);
     }
 
+
     for (int i = 0; word[i] != 0; i++) {
-        printf("%d", i);
         letter_found_flag = 0;
         for (int j = 0; j < 7; j++) {
-            for (int k = 0; k < (int)sizeof(letters[i]); k++) {
+            for (int k = 0; letters[j][k] != 0; k++) {
                 if (word[i] == letters[j][k]) {
                     value_of_word += values[j];
                     letter_found_flag = 1;
@@ -46,15 +44,6 @@ int main(void) {
             }
         }
     }
-
-    //for letter in word
-        //for letter value string
-            //for letter in letter value string
-                //check if same
-                //if same
-                    //break to
-
-    //here
 
     printf("Word value: %d\n", value_of_word);
     return(0);
